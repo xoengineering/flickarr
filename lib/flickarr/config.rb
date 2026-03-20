265393a79acaf407289b8fd548b7230c84f514d1
Shane Becker <veganstraightedge@gmail.com>
2026-03-19 19:22:09 -0700

    Add Config save/load with YAML file persistence

--- lib/flickarr/config.rb
+++ lib/flickarr/config.rb
@@ -1,6 +1,11 @@
+require 'fileutils'
+require 'yaml'
+
 module Flickarr
   class Config
-    attr_accessor :api_key, :shared_secret, :access_token, :access_secret, :user_nsid, :username
+    ATTRIBUTES = %i[api_key shared_secret access_token access_secret user_nsid username].freeze
+
+    attr_accessor(*ATTRIBUTES)
 
     def initialize
       @api_key = ENV.fetch('FLICKARR_API_KEY', nil)
@@ -9,6 +14,30 @@ module Flickarr
       @access_secret = nil
       @user_nsid = nil
       @username = nil
+    end
+
+    def save path
+      FileUtils.mkdir_p(File.dirname(path))
+      File.write(path, YAML.dump(to_h))
+    end
+
+    def to_h
+      ATTRIBUTES.to_h { |attr| [attr.to_s, public_send(attr)] }
+    end
+
+    def self.load path
+      config = new
+      return config unless File.exist?(path)
+
+      yaml = YAML.load_file(path)
+      return config unless yaml.is_a?(Hash)
+
+      ATTRIBUTES.each do |attr|
+        value = yaml[attr.to_s]
+        config.public_send(:"#{attr}=", value) if value
+      end
+
+      config
     end
   end
 end
--- spec/flickarr/config_spec.rb
+++ spec/flickarr/config_spec.rb
@@ -1,3 +1,5 @@
+require 'tmpdir'
+
 RSpec.describe Flickarr::Config do
   describe '#initialize' do
     it 'has nil attributes by default' do
@@ -29,6 +31,76 @@ RSpec.describe Flickarr::Config do
       config = described_class.new
 
       expect(config.shared_secret).to eq('env-secret')
+    end
+  end
+
+  describe '#save' do
+    it 'writes config to a YAML file' do
+      path = File.join(Dir.tmpdir, "flickarr-test-#{Process.pid}", 'config.yml')
+      config = described_class.new
+      config.api_key = 'test-key'
+      config.shared_secret = 'test-secret'
+
+      config.save(path)
+
+      yaml = YAML.load_file(path)
+      expect(yaml['api_key']).to eq('test-key')
+      expect(yaml['shared_secret']).to eq('test-secret')
+    ensure
+      FileUtils.rm_rf(File.dirname(path))
+    end
+  end
+
+  describe '.load' do
+    it 'reads config from a YAML file' do
+      dir = File.join(Dir.tmpdir, "flickarr-test-#{Process.pid}")
+      path = File.join(dir, 'config.yml')
+      FileUtils.mkdir_p(dir)
+      data = {
+        'api_key'       => 'file-key',
+        'shared_secret' => 'file-secret',
+        'access_token'  => 'file-token',
+        'access_secret' => 'file-access-secret',
+        'user_nsid'     => '12345@N00',
+        'username'      => 'testuser'
+      }
+      File.write(path, YAML.dump(data))
+
+      config = described_class.load(path)
+
+      expect(config.api_key).to eq('file-key')
+      expect(config.shared_secret).to eq('file-secret')
+      expect(config.access_token).to eq('file-token')
+      expect(config.access_secret).to eq('file-access-secret')
+      expect(config.user_nsid).to eq('12345@N00')
+      expect(config.username).to eq('testuser')
+    ensure
+      FileUtils.rm_rf(dir)
+    end
+
+    it 'falls back to env vars when file values are missing' do
+      dir = File.join(Dir.tmpdir, "flickarr-test-#{Process.pid}")
+      path = File.join(dir, 'config.yml')
+      FileUtils.mkdir_p(dir)
+      File.write(path, YAML.dump('username' => 'testuser'))
+
+      allow(ENV).to receive(:fetch).and_call_original
+      allow(ENV).to receive(:fetch).with('FLICKARR_API_KEY', nil).and_return('env-key')
+      allow(ENV).to receive(:fetch).with('FLICKARR_SHARED_SECRET', nil).and_return(nil)
+
+      config = described_class.load(path)
+
+      expect(config.api_key).to eq('env-key')
+      expect(config.username).to eq('testuser')
+    ensure
+      FileUtils.rm_rf(dir)
+    end
+
+    it 'returns a default config when file does not exist' do
+      config = described_class.load('/tmp/nonexistent-flickarr-config.yml')
+
+      expect(config.api_key).to be_nil
+      expect(config.username).to be_nil
     end
   end
 end
