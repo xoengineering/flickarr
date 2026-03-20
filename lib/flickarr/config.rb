d39b38510404271541f848a704fbed25de19a1e2
Shane Becker <veganstraightedge@gmail.com>
2026-03-19 19:32:45 -0700

    Use symbol keys in Config#to_h, convert to/from strings for YAML

--- lib/flickarr/config.rb
+++ lib/flickarr/config.rb
@@ -15,18 +15,19 @@ module Flickarr
     end
 
     def save path
+      string_hash = to_h.transform_keys(&:to_s)
       FileUtils.mkdir_p(File.dirname(path))
-      File.write(path, YAML.dump(to_h))
+      File.write(path, YAML.dump(string_hash))
     end
 
     def to_h
       {
-        'api_key'       => api_key,
-        'shared_secret' => shared_secret,
-        'access_token'  => access_token,
-        'access_secret' => access_secret,
-        'user_nsid'     => user_nsid,
-        'username'      => username
+        api_key:       api_key,
+        shared_secret: shared_secret,
+        access_token:  access_token,
+        access_secret: access_secret,
+        user_nsid:     user_nsid,
+        username:      username
       }
     end
 
@@ -34,15 +35,15 @@ module Flickarr
       config = new
       return config unless File.exist?(path)
 
-      yaml = YAML.load_file(path)
+      yaml = YAML.load_file(path, symbolize_names: true)
       return config unless yaml.is_a?(Hash)
 
-      config.api_key       = yaml['api_key']       if yaml['api_key']
-      config.shared_secret = yaml['shared_secret'] if yaml['shared_secret']
-      config.access_token  = yaml['access_token']  if yaml['access_token']
-      config.access_secret = yaml['access_secret'] if yaml['access_secret']
-      config.user_nsid     = yaml['user_nsid']     if yaml['user_nsid']
-      config.username      = yaml['username']      if yaml['username']
+      config.api_key       = yaml[:api_key]       if yaml[:api_key]
+      config.shared_secret = yaml[:shared_secret] if yaml[:shared_secret]
+      config.access_token  = yaml[:access_token]  if yaml[:access_token]
+      config.access_secret = yaml[:access_secret] if yaml[:access_secret]
+      config.user_nsid     = yaml[:user_nsid]     if yaml[:user_nsid]
+      config.username      = yaml[:username]      if yaml[:username]
 
       config
     end
